a22575cc6f4573139328fccc0adae719cd12a55d
99063871 <99063871@mydavinci.com>
2021-11-03 10:07:26 +0100

    code bijgewerkt, heb de varstr weg gehaald en het in de print gezet(eerst wist ik niet dat dat kon)

--- duizelig/tellen.py
+++ duizelig/tellen.py
@@ -2,9 +2,6 @@ i = 50
 x = 0
 anwser = 0
 while anwser < 1000:
-    istr = str(i)
     x = x + 50
-    xstr = str(x)
     anwser = i+x
-    anwserstr = str(anwser)
-    print(xstr + " + " + istr + " = " + anwserstr)
+    print(x, " + ", i, " = ", anwser)
